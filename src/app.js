7de1be3407bbf358318c451bcbbc1391fedc9e71
ChandanE38 <ck236385@gmail.com>
2024-10-23 15:16:52 +0530

    Operation Done Successfull of GET ,PUSH ,DELETE

--- src/app.js
+++ src/app.js
@@ -4,12 +4,20 @@ const app=express();
 
 
 
-app.use("/hello",(req, res) => {
-    res.send("hello world 2");
+app.get("/hello",(req, res) => {
+    res.send("we get hello world 2");
+}); 
+
+app.post("/test",(req, res) => {
+    res.send("We tested hello world 3");
+}); 
+
+app.delete("/test",(req, res) => {
+    res.send("We have Deleted successfully 3");
 }); 
 
 app.use("/test",(req, res) => {
-    res.send("hello world 3");
+    res.send("hello world");
 }); 
 
 app.listen(3000 , ()=>{
